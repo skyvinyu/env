d1263579ba2e262954da391458f357b1fd431759
Skyvin <skyvin.yu@gmail.com>
2017-11-10 11:27:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sSL https://github.com/skyvin/env/archive/master.zip 
+curl -LOk https://github.com/skyvinyu/env/archive/master.zip
 #| tar --strip-components=1 -C ~ -zx
 unzip master.zip
 mv env-master/.* ~
